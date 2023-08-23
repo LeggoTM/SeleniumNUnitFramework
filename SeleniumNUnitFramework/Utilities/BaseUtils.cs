2aec9ae169729f9460a2e2d5330ab4785b202683
LeggoTM <m.tanmay17@gmail.com>
2023-08-23 07:45:05 +0530

    Refactor

--- SeleniumNUnitFramework/Utilities/BaseUtils.cs
+++ SeleniumNUnitFramework/Utilities/BaseUtils.cs
@@ -1,28 +1,17 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
-using System.Configuration;
 
 namespace SeleniumNUnitFramework.Utilities
 {
-    public enum BrowserType
-    {
-        Chrome,
-        Firefox,
-        Edge
-    }
-    
+
     internal class BaseUtils
     {
-        //public ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
-        protected static IWebDriver driver;
+        protected IWebDriver driver;
 
         public static void WaitUntilElementIsDisplayed(IWebDriver driver, By locator, int timeoutInSeconds = 10)
         {
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-                wait.Until(driver => driver.FindElement(locator).Displayed);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            wait.Until(driver => driver.FindElement(locator).Displayed);
         }
     }
 }
